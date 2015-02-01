bde3768a5f18a65512ec4ae7a6dfc9debfe3e0ed
Kunal Rajani <kr386@cornell.edu>
2015-02-01 00:38:27 -0500

    second commit
    just pressed an enter

--- data_explore_v1.py
+++ data_explore_v1.py
@@ -13,6 +13,8 @@ as the name is changed.
   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
 
  0. You just DO WHAT THE FUCK YOU WANT TO.
+
+just checking to see how commits work
 '''
 
 
